6b4f0b93614d532fda3df632ee5feec7bce7abac
oshada20056 <153474645+oshada20056@users.noreply.github.com>
2024-08-08 23:49:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -970,12 +970,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
-          
+           InputMediaPhoto(random.choice(PICS))
         )
         await query.message.edit_text(
             text=script.HELP_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
+            
         )
     elif query.data == "about":
         buttons = [[
